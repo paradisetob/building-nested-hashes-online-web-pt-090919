663e47d0ceee4cd296a9703af32bf6747a87407a
tracey brimage <onelovenilynn@gmail.com>
2019-10-01 17:07:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,10 +4,10 @@ def third_challenge
       :patriarch => {
         name: "Lord Montague", age: "53" },
       :matriarch => {
-        #your key/value pairs here
+      {name: "Lady Montague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        
         },
       :hero_friends => []
    },
