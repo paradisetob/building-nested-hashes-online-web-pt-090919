42f4332aa03bf7b6339475b7ee9016ae45326752
tracey brimage <onelovenilynn@gmail.com>
2019-10-01 17:09:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,11 +13,11 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        
+        name: "Lord Capulet", age: "50"
       },
 
       :matriarch => {
-        #your key/value pairs here
+
         },
       :heroine => {
         #your key/value pairs here
